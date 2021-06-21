4f574f5b70144b5bf58cc15cce626613a302b022
sumin <kim.sumin.wat2014@gmail.com>
2021-06-21 16:45:52 -0700

    set the function which needs in game.rb file

--- main.rb
+++ main.rb
@@ -4,3 +4,11 @@
 require "pp"
 require "./player"
 require "./game"
+
+p1 = Player.new(3, 'Player 1')
+p2 = Player.new(3, 'Player 2')
+
+game = Game.new(p1, p2)
+
+game.create_game()
+# game.player1
