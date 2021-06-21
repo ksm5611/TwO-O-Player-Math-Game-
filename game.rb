34ee0dcfe3d2e280e836e10c55bfd2eda8c470c0
sumin <kim.sumin.wat2014@gmail.com>
2021-06-21 15:28:31 -0700

    set the p1 and p2 and adding question

--- game.rb
+++ game.rb
@@ -6,4 +6,25 @@ class Game
     @player2 = player2
     @current_player = player1
   end
+
+  def create_game() 
+    @player
+    if @current_player == @player1
+      @player = 'Player 1'
+    elsif @current_player == @player2
+      @player = "Player 2"
+    end
+    num1 = rand(1..20)
+    num2 = rand(1..20)
+    puts "#{@player}: What does #{num1} plus #{num2} equal?"
+    answer = gets.chomp
+    puts num1 + num2 
+    puts answer
+    if num1 + num2 == answer.to_i
+      puts "YES! You are correct."
+    else 
+      puts "Seriously? No!"
+    end
+
+  end
 end
