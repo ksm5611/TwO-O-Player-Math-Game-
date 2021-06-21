f34b3b57c112af882120b5c4285571d4865be6f5
sumin <kim.sumin.wat2014@gmail.com>
2021-06-21 16:42:23 -0700

    output correct score with new turn message

--- game.rb
+++ game.rb
@@ -1,5 +1,5 @@
 # In this file we define the methods to help game class
-
+require "pp"
 class Game 
   def initialize(player1, player2)
     @player1 = player1
@@ -7,24 +7,26 @@ class Game
     @current_player = player1
   end
 
-  def create_game() 
-    @player
-    if @current_player == @player1
-      @player = 'Player 1'
-    elsif @current_player == @player2
-      @player = "Player 2"
-    end
-    num1 = rand(1..20)
-    num2 = rand(1..20)
-    puts "#{@player}: What does #{num1} plus #{num2} equal?"
-    answer = gets.chomp
-    puts num1 + num2 
-    puts answer
-    if num1 + num2 == answer.to_i
-      puts "YES! You are correct."
-    else 
-      puts "Seriously? No!"
+  def create_game()
+
+      num1 = rand(1..20)
+      num2 = rand(1..20)
+      puts "#{@current_player.name()}: What does #{num1} plus #{num2} equal?"
+      answer = gets.chomp
+      if num1 + num2 == answer.to_i
+        puts "YES! You are correct."
+      else 
+        puts "Seriously? No!"
+        @current_player.lose_lives();
+      end
+      
+      puts "P1: #{@player1.get_lives()} vs P2: #{@player2.get_lives()}"
+      puts "----- NEW TRUN -----"
+      if @current_player == @player2
+        @current_player = @player1
+      else
+        @current_player = @player2
+      end  
     end
 
-  end
 end
