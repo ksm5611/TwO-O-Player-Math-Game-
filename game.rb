367f75cd7ffb8899cf248279f7f9685b93f05dc6
sumin <kim.sumin.wat2014@gmail.com>
2021-06-21 15:07:17 -0700

    set the game class

--- game.rb
+++ game.rb
@@ -1,1 +1,9 @@
 # In this file we define the methods to help game class
+
+class Game 
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+    @current_player = player1
+  end
+end
